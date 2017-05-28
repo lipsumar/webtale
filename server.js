1897c330ba30d06b0334ca570d9a8097fd236973
Emmanuel Pire <piremmanuel@gmail.com>
2017-05-28 17:49:54 +0200

    use correct port

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.post('/register', (req, res) => {
 	})
 })
 
-app.listen(80, function () {
-	console.log('Example app listening on port 3000!')
+app.listen((process.env.PORT || 3000), function () {
+	console.log('app is listening')
 })
