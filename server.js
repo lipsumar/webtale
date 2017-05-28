410563a54afaf7d8af2698c2cf9308f4f83256e1
Emmanuel Pire <piremmanuel@gmail.com>
2017-05-28 17:41:55 +0200

    test user table

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+app.use( require('body-parser').json() );
+
+app.get('/', function (req, res) {
+	res.send('This is WebTale')
+})
+
+app.post('/register', (req, res) => {
+	users.findByEmail(req.body.email, (err, user) => {
+		if(err) return res.send(err)
+		if(user){
+			res.send(user)
+		}else{
+			res.send('we don’t have that')
+		}
+	})
+})
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
+})
