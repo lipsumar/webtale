3511cd49efd487d0e19558c3b59f18756d106403
Emmanuel Pire <piremmanuel@gmail.com>
2017-05-28 18:08:34 +0200

    debug

--- server.js
+++ server.js
@@ -10,16 +10,21 @@ app.get('/', function (req, res) {
 })
 
 app.post('/register', (req, res) => {
+	console.log('/register');
 	users.findByEmail(req.body.email, (err, user) => {
 		if(err) {
+			console.log('err 1');
 			res.status(500).send(err)
 			return
 		}
 
 		if(user){
+			console.log('got user');
 			res.send(user)
 		}else{
+			console.log('no user');
 			users.add(req.body.email, (err, user) => {
+				console.log('after add', err, user);
 				if(err){
 					res.status(500).send(err);
 				}else{
