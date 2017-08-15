7aa4580d953d9991c42bb761bc8e516dffb19058
Emmanuel Pire <piremmanuel@gmail.com>
2017-08-15 15:44:49 +0200

    ignore files starting with .

--- compile.js
+++ compile.js
@@ -58,6 +58,9 @@ pageBuilder.init(options, err => {
         if(relativePath.substr(0, 1) === '_'){
             return cb();
         }
+	    if(relativePath.substr(0,1)==='.'){
+            return cb();
+        }
 
         console.log(relativePath);
 
